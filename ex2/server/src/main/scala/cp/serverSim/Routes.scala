28d2ea098020bceb53fcfbb9474b6211547363d0
Gonçalo Branquinho <goncalobranquinhom@gmail.com>
2026-05-22 01:07:07 +0100

    exercise 2.2

--- ex2/server/src/main/scala/cp/serverSim/Routes.scala
+++ ex2/server/src/main/scala/cp/serverSim/Routes.scala
@@ -8,66 +8,79 @@ import org.slf4j.LoggerFactory
 object Routes {
   // Logger object, printing to the file logs/logs.txt
   private val logger = LoggerFactory.getLogger(getClass)
-  private val state = new ServerState()
+  private val state = new ServerState(6)
 
-  val routes: IO[HttpRoutes[IO]] =
-   IO{HttpRoutes.of[IO] {
+  val routes: IO[HttpRoutes[IO]] = {
+    IO{HttpRoutes.of[IO] {
 
-     // React to a "status" request
-     case GET -> Root / "status" =>
-       Ok(state.toHtml)
-         .map(addCORSHeaders)
-         .map(_.withContentType(org.http4s.headers.`Content-Type`(MediaType.text.html)))
+      // React to a "status" request
+      case GET -> Root / "status" =>
+        Ok(state.toHtml)
+          .map(addCORSHeaders)
+          .map(_.withContentType(org.http4s.headers.`Content-Type`(MediaType.text.html)))
 
-     // React to a "reset" request
-    case GET -> Root / "reset" =>
-      state.counter = 0
-      Ok("State reset!")
-        .map(addCORSHeaders)
+          // React to a "reset" request
+      case GET -> Root / "reset" =>
+        state.counter.set(0)
+        state.content.set(Array.empty[String])
+        Ok("State reset!")
+          .map(addCORSHeaders)
 
-     // React to a "run-simulation" request
-     case req@GET -> Root / "run-simulation" =>
-       val cmdOpt = req.uri.query.params.get("cmd")
-       val userIp = req.remoteAddr.getOrElse("unknown")
+          // React to a "run-simulation" request
+      case req@GET -> Root / "run-simulation" =>
+        val cmdOpt = req.uri.query.params.get("cmd")
+        val userIp = req.remoteAddr.getOrElse("unknown")
 
-       //// printing to the terminal instead of a logging file
-       //println(">>> got run-simulation!")
-       //println(s">>> Cmd: ${cmdOpt}")
-       //println(s">>> userIP: $userIp")
+        //// printing to the terminal instead of a logging file
+        //println(">>> got run-simulation!")
+        //println(s">>> Cmd: ${cmdOpt}")
+        //println(s">>> userIP: $userIp")
 
-       cmdOpt match {
-         case Some(cmd) =>
-          // calling the `runProcess` method, which simulates running a process
-           Ok(runProcess(cmd, userIp.toString))
-             .map(addCORSHeaders)
+        cmdOpt match {
+          case Some(cmd) =>
+            // calling the `runProcess` method, which simulates running a process
+            Ok(runProcess(cmd, userIp.toString))
+              .map(addCORSHeaders)
 
-         case None =>
-           BadRequest("⚠️ Command not provided. Use /run-simulation?cmd=<your_commands>")
-             .map(addCORSHeaders)
-       }
-   }}
+          case None =>
+            BadRequest("⚠️ Command not provided. Use /run-simulation?cmd=<your_commands>")
+              .map(addCORSHeaders)
+        }
+    }
+    }
+  }
+
+
+  def asynchr(s: String) = {
+    state.tasks.synchronized {
+      state.tasks.enqueue(s)
+      // now notifying
+      state.tasks.notify()
+    }
+  }
 
 
   /** Run a given process and collect its output. */
   /**
-    * This method simulates running a process. It should be replaced with actual code
-    * to simulate the process using a thread pool. The `Thread.sleep` is just mimicking
-    * the time to process the comand, and should be removed.
-    *
-    * @param cmd the command to run, which can be a single command or multiple commands separated by ";"
-    * @param userIp the IP address of the user who sent the request, used for logging purposes
-    * @return a string confirming the received command and user IP, which will be sent back to the client as a response
-    */
+   * This method simulates running a process. It should be replaced with actual code
+   * to simulate the process using a thread pool. The `Thread.sleep` is just mimicking
+   * the time to process the comand, and should be removed.
+   *
+   * @param cmd the command to run, which can be a single command or multiple commands separated by ";"
+   * @param userIp the IP address of the user who sent the request, used for logging purposes
+   * @return a string confirming the received command and user IP, which will be sent back to the client as a response
+   */
+
   private def runProcess(cmd: String, userIp: String): String = {
-    state.counter += 1
-    val cnt = state.counter
+    val cnt = state.counter.incrementAndGet()
     val cmds = cmd.split(";").map(_.trim).filter(_.nonEmpty)
+    cmds.foreach(asynchr)
+
     // Printing the received command and user IP to the logs
     logger.info(s"🔹 Starting processes (${cnt}) for user $userIp:" +
       s"${cmds.map("\n - "+_).mkString}")
 
     // TODO:Run process here. The `Thread.sleep` should be removed.
-    Thread.sleep(500)
     val output: String = s"[${cnt}] Received request from $userIp: ${cmds.mkString(" | ")}"
 
     output
--- ex2/server/src/main/scala/cp/serverSim/ServerState.scala
+++ ex2/server/src/main/scala/cp/serverSim/ServerState.scala
@@ -1,10 +1,61 @@
 package cp.serverSim
+import scala.collection._
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-class ServerState() {
+class Worker(
+    tasks: mutable.Queue[String],
+    content: AtomicReference[Array[String]]
+) extends Thread {
+  setDaemon(true)
+  def poll() = tasks.synchronized {
+    while (tasks.isEmpty) tasks.wait()
+    // now using wait
+    tasks.dequeue()
+  }
+
+  override def run() = while (true) {
+    val task = poll()
+    val args = task
+      .split(" ")
+      .map(_.trim)
+      .filter(_.nonEmpty) // não separar por espaços desta forma
+    var delay = 0
+    if (args.size >= 4 && args(3) == "@") {
+      delay = args(3).toInt
+    }
+    Thread.sleep(delay * 1000)
+
+    val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
+    val element =
+      "[" ++ LocalDateTime.now().format(formatter) ++ "] " ++ args(1)
+    content.updateAndGet { old =>
+      old :+ element
+    }
+
+  }
+}
+
+class ServerState(n: Int) {
   // TODO: extend the state of the server
 
-  var counter = 0;
+  val counter = new AtomicInteger(0)
+  val content = new AtomicReference[Array[String]](Array.empty[String])
 
-  def toHtml: String =
-    s"<p><strong>counter:</strong> $counter</p>"
+  val tasks = mutable.Queue[String]()
+  (0 until n).map(i => {
+    val worker = new Worker(tasks, content)
+    worker.start()
+    worker
+  })
+
+  def toHtml: String = {
+    val aux = content.get()
+    s"<p><strong>counter:</strong> ${counter.get()}</p>" +
+      s"<p><strong>content:</strong></p>${if (aux.isEmpty) ""
+        else aux.mkString("<br>")}"
+
+  }
 }
